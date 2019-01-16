7dfcecdd540e8a849ab57c3208a52dd36a5bd41e
Dev <dorian.denicola@siicanada.com>
2019-01-16 09:46:27 -0500

    ArticleControllerTest modified to be able to execute all tests in once

--- src/test/java/com/siicanada/article/controller/ArticleControllerTest.java
+++ src/test/java/com/siicanada/article/controller/ArticleControllerTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -22,9 +23,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(ArticleController.class)
 @AutoConfigureMockMvc
-public class ArticleEntityControllerTest {
+public class ArticleControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
